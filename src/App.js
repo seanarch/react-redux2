958fdc565b035482c6b83d89faee891e96a8ffae
seanarch <zhuqingyun@outlook.com>
2023-06-16 14:06:55 -0600

    Add contents on todos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,48 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Todo List app</header>
+      <header className="App-header">
+        <h2>Todo List App in Redux</h2>
+        <form>
+          <input
+            placeholder="Enter a Todo"
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: "none",
+              fontSize: 20,
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              padding: 12,
+              borderRadius: 25,
+              fontSize: 15,
+              marginLeft: 20,
+            }}
+          >
+            Go
+          </button>
+        </form>
+        <ul className="allTodos">
+          <li className="singleTodo">
+            <span>First Todo</span>
+            <button
+              style={{
+                padding: 10,
+                borderRadius: 25,
+                border: "1px solid white",
+                color: "white",
+                backgroundColor: "orangered",
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
